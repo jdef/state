841c91b4549b3541fb5c81fe6f560cfbfbd4fd71
James DeFelice <james@mesosphere.io>
2016-03-09 06:16:55 +0000

    make iface a required parameter, add autogen banner; move helpers.go to helpers_generated.go

--- demo/agent/agent.go
+++ demo/agent/agent.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//go:generate gosm -o helpers.go
+//go:generate gosm -o helpers_generated.go -iface Interface
 
 package agent
 
